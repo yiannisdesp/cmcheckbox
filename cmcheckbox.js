5ddc0406dcb17850d3b32a01f90af0ef75df40a8
yiannisdesp <ioannis.despotis42@gmail.com>
2016-10-05 13:47:30 +0300

    fixed checkbox element state

--- cmcheckbox.js
+++ cmcheckbox.js
@@ -6,27 +6,27 @@
  */
 (function ( $ ) {
 
-	$.fn.cmcheckbox = function( options ) {
+    $.fn.cmcheckbox = function( options ) {
 
-		// Initialize default options
+        // Initialize default options
         var settings = $.extend({
             // checked state
-        	checkedColor: "#fff",
+            checkedColor: "#fff",
             checkedBackgroundColor: "transparent",
             checkedBorderWidth: "2px",
             checkedBorderColor : "#556b2f",
             checkedFaClass : "fa-check",
             checkedFaSize : "23px",
             // unchecked state
-        	uncheckedColor: "#fff",
+            uncheckedColor: "#fff",
             uncheckedBackgroundColor: "#fff",
             uncheckedBorderWidth: "2px",
             uncheckedBorderColor : "#556b2f",
             uncheckedFaClass : "",
             uncheckedFaSize : "25px",
             // general
-			width : "25px",
-			height : "25px",
+            width : "25px",
+            height : "25px",
             // callbacks
             onCheck : function() {},
             onUncheck : function() {},
@@ -36,75 +36,75 @@
         var headStyleClasses = [];
         var headCss = "";
         function generateHeadCss(className, css){
-        	headStyleClasses.push({className,css});
-        	$.each(headStyleClasses, function(index, val){
-        		headCss += val.className+"{" + val.css + "}";
-        	});
-        	$("head").find("style[data-cmcheckbox='true']").remove();
-        	$("head").append("<style data-cmcheckbox='true'>"+headCss+"</style>");
+            headStyleClasses.push({className,css});
+            $.each(headStyleClasses, function(index, val){
+                headCss += val.className+"{" + val.css + "}";
+            });
+            $("head").find("style[data-cmcheckbox='true']").remove();
+            $("head").append("<style data-cmcheckbox='true'>"+headCss+"</style>");
         }
         
         // generate checkbox style
         function getCheckboxStyle(state){
-        	var style = 'display:block;float:left;margin:0;margin-right:5px;position:relative;';
-        	if(state == 'checked') {
-        		style += 'width:'+settings.width+';';
-	        	style += 'height:'+settings.height+';';
-	        	style += 'border:'+settings.checkedBorderWidth+' solid '+settings.checkedBorderColor+';';
-	        	style += 'background-color:'+settings.checkedBackgroundColor+';';
-        	}else{
-        		style += 'width:'+settings.width+';';
-	        	style += 'height:'+settings.height+';';
-	        	style += 'border:'+settings.uncheckedBorderWidth+' solid '+settings.uncheckedBorderColor+';';
-	        	style += 'background-color:'+settings.uncheckedBackgroundColor+';';
-        	}
-        	return style;
+            var style = 'display:block;float:left;margin:0;margin-right:5px;position:relative;';
+            if(state == 'checked') {
+                style += 'width:'+settings.width+';';
+                style += 'height:'+settings.height+';';
+                style += 'border:'+settings.checkedBorderWidth+' solid '+settings.checkedBorderColor+';';
+                style += 'background-color:'+settings.checkedBackgroundColor+';';
+            }else{
+                style += 'width:'+settings.width+';';
+                style += 'height:'+settings.height+';';
+                style += 'border:'+settings.uncheckedBorderWidth+' solid '+settings.uncheckedBorderColor+';';
+                style += 'background-color:'+settings.uncheckedBackgroundColor+';';
+            }
+            return style;
         }
 
         // generate font awesome style
         function getSymbolStyle(state){
-        	var style = 'position:absolute;top:0;left:0;right:0;bottom:0;text-align:center;margin:auto;';
-        	if(state == 'checked') {
-        		style += 'color:'+settings.checkedColor+';';
-	        	style += 'font-size:'+settings.checkedFaSize+';';
-        	}else{
-        		style += 'color:'+settings.checkedColor+';';
-	        	style += 'font-size:'+settings.checkedFaSize+';';
-        	}
-        	return style;
+            var style = 'position:absolute;top:0;left:0;right:0;bottom:0;text-align:center;margin:auto;';
+            if(state == 'checked') {
+                style += 'color:'+settings.checkedColor+';';
+                style += 'font-size:'+settings.checkedFaSize+';';
+            }else{
+                style += 'color:'+settings.checkedColor+';';
+                style += 'font-size:'+settings.checkedFaSize+';';
+            }
+            return style;
         }
 
         // style custom checkbox based on state
         function styleCheckbox(state, $checkboxContainer){
-        	$checkboxContainer.children("span.cm-generated-checkbox").remove();
-        	if(state == 'checked'){
-        		style = getCheckboxStyle('checked');
-	        	$checkboxContainer.prepend('<span class="cm-generated-checkbox checked" style="'+style+'"><i style="'+getSymbolStyle('checked')+'" class="fa '+settings.checkedFaClass+'"><i></span>');
-        	}else{
-        		style = getCheckboxStyle('unchecked');
-	        	$checkboxContainer.prepend('<span class="cm-generated-checkbox unchecked" style="'+style+'"><i style="'+getSymbolStyle('unchecked')+'" class="fa '+settings.uncheckedFaClass+'"><i></span>');
-        	}
+            $checkboxContainer.children("span.cm-generated-checkbox").remove();
+            if(state == 'checked'){
+                style = getCheckboxStyle('checked');
+                $checkboxContainer.prepend('<span class="cm-generated-checkbox checked" style="'+style+'"><i style="'+getSymbolStyle('checked')+'" class="fa '+settings.checkedFaClass+'"><i></span>');
+            }else{
+                style = getCheckboxStyle('unchecked');
+                $checkboxContainer.prepend('<span class="cm-generated-checkbox unchecked" style="'+style+'"><i style="'+getSymbolStyle('unchecked')+'" class="fa '+settings.uncheckedFaClass+'"><i></span>');
+            }
         }
 
         // custom checkbox click event
         function attachEvent(elementIdentifier){
-        	$('body').on("click", elementIdentifier + " span", function(e){
+            $('body').on("click", elementIdentifier + " span", function(e){
                 var $customCheckbox = $(this).closest(elementIdentifier);
-        		var $checkbox = $customCheckbox.children("input[type='checkbox']");
-	        	if($checkbox[0].checked){
+                var $checkbox = $customCheckbox.children("input[type='checkbox']");
+                if($checkbox.is(':checked')){
                     // style custom checkbox and uncheck native
-	        		$checkbox.prop('checked', true);
-	        		styleCheckbox('unchecked', $customCheckbox);
+                    $checkbox.prop('checked', true);
+                    styleCheckbox('unchecked', $customCheckbox);
                     // on uncheck callback
                     settings.onUncheck.call( $customCheckbox );
-	        	}else{
+                }else{
                     // style custom checkbox and check native
-	        		$checkbox.prop('checked', false);
-	        		styleCheckbox('checked', $customCheckbox);
+                    $checkbox.prop('checked', false);
+                    styleCheckbox('checked', $customCheckbox);
                     // on check callback
                     settings.onCheck.call( $customCheckbox );
-	        	}
-	        });
+                }
+            });
         }
 
         var $checkboxContainer; // checkbox jQuery container
@@ -113,28 +113,28 @@
         var bindingElements = [];
         // transform native checkboxes
         this.each(function() {
-	        // Convert native checkbox to all attached classes or ids
-	        $checkboxContainer = $(this);
-	        $checkbox = $checkboxContainer.children("input[type='checkbox']");
-	        $checkbox.css({"display" : "none"});
-	        // build checkbox style based on settings
-	        if($checkbox.is(":checked")){
-	        	styleCheckbox('checked', $checkboxContainer);
-	        }else{
-	        	styleCheckbox('unchecked', $checkboxContainer);
-	        }
-        	// identify class or ID
-        	var IDAttribute = $(this).attr('id');
-        	var className = ($(this).attr("class")).replace(new RegExp(" ", 'g'), ".");
-			// For some browsers, 'attr' is undefined; for others, 'attr' is false.  Check for both.
-			if (typeof IDAttribute !== typeof undefined && IDAttribute !== false) {
-			    // take ID
+            // Convert native checkbox to all attached classes or ids
+            $checkboxContainer = $(this);
+            $checkbox = $checkboxContainer.children("input[type='checkbox']");
+            $checkbox.css({"height" : 0, "position" : "absolute", "visibility" : "hidden"});
+            // build checkbox style based on settings
+            if($checkbox.is(":checked")){
+                styleCheckbox('checked', $checkboxContainer);
+            }else{
+                styleCheckbox('unchecked', $checkboxContainer);
+            }
+            // identify class or ID
+            var IDAttribute = $(this).attr('id');
+            var className = ($(this).attr("class")).replace(new RegExp(" ", 'g'), ".");
+            // For some browsers, 'attr' is undefined; for others, 'attr' is false.  Check for both.
+            if (typeof IDAttribute !== typeof undefined && IDAttribute !== false) {
+                // take ID
                 bindingElements.push("#" + IDAttribute) // attach event by id
-			}else{
-				// take class
+            }else{
+                // take class
                 bindingElements.push("." + className) // attach event by class
-			}
-	    });
+            }
+        });
 
         if(bindingElements.length > 0){
             var uniqueBindingElements = []; // filter out unique elements
@@ -148,6 +148,6 @@
             });    
         }
 
-	};
+    };
 
 }( jQuery ));
